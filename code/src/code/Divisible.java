9b216f437c30aa07fbab5e4e6d82cc322aa07c29
hyungjoon <hyungjoon6876@gmail.com>
2018-04-17 13:09:41 +0900

    algorithm study

---
+++ code/src/code/Divisible.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Divisible {
+	public int[] divisible(int[] array, int divisor) {
+		//ret에 array에 포함된 정수중, divisor로 나누어 떨어지는 숫자를 순서대로 넣으세요.
+		int[] ret = IntStream.of(array).filter(x -> x%divisor == 0).toArray();
+		return ret;
+	}
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void main(String[] args) {
+		Divisible div = new Divisible();
+		int[] array = {5, 9, 7, 10};
+		System.out.println( Arrays.toString( div.divisible(array, 5) ));
+	}
+}
